5d155df0423f446258d2af334be01fddae700835
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-10-06 02:01:51 +0000

    fix

--- ICS3U-Assignment3.py
+++ ICS3U-Assignment3.py
@@ -7,7 +7,8 @@
 
 def main():
     # input
-    number = int(input("Type in any number that you want to see go through the 3X + 1 loop: "))
+    initial_number = int(input("Type in any number that you want to see go through the 3X + 1 loop: "))
+    number = initial_number
     step = 0
 
     # process
@@ -17,17 +18,16 @@ def main():
 
     while number > 1:
         if number % 2:
-            print("{:,} × 3 + 1 =".format(number),end="")
+            print("{:,} × 3 + 1 =".format(number), end="")
             number = number * 3 + 1
-            print(" {:,}".format(number))
         else:
-            print("{:,} : 2 =".format(number),end="")
+            print("{:,} : 2 =".format(number), end="")
             number = int(number / 2)
-            print(" {:,}".format(number))
-        step = step + 1
+        print(" {:,}".format(number))
+        step += 1
         print("Step {:,}\n".format(step))
-    print("\nThe number has stopped on the 1 and 2 infinity loop.")
-    print("\n(1×3+1=4, 4:2=2, 2:2=1)")
+    print("\nThe 3X + 1 problem has been resolved in ", end="")
+    print("{0:,} steps, starting from number {1:,}.".format(step, initial_number))
 
     print("\n\nDone.")
     return
